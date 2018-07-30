a5748f9bc617ac7c9818579e22c6eaeb5218c30b
Syrou <syroumusic@gmail.com>
2018-07-30 18:11:26 +0200

    Waka

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DatContainer_1 = require("./src/DatContainer");
-var path = require("path");
-var testPath = path.join(process.cwd(), "assets", "/test");
-var datContainer = new DatContainer_1.DatContainer(testPath, "AbyssObjects.dat", function (instance) {
-    instance.SaveToJson();
-});
